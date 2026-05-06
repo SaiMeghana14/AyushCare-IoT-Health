6a83af6ec46826d6a2d33a53ad5a9183e77b4486
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-05-06 19:12:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
 import streamlit as st
+
 from pages.login import page_login
 from pages.register import page_register
 from pages.dashboard import page_dashboard
 
+# --------------------------------------------------------------
+# PAGE CONFIG
+# --------------------------------------------------------------
 st.set_page_config(
     page_title="AyushCare",
     page_icon="🩺",
     layout="wide"
 )
 
+# --------------------------------------------------------------
+# SESSION STATE
+# --------------------------------------------------------------
 if "page" not in st.session_state:
     st.session_state.page = "register"
 
 if "current_user" not in st.session_state:
     st.session_state.current_user = None
 
-# Navigation
+# --------------------------------------------------------------
+# NAVIGATION
+# --------------------------------------------------------------
 if st.session_state.page == "register":
+
     page_register()
 
 elif st.session_state.page == "login":
+
     page_login()
 
 elif st.session_state.page == "dashboard":
+
     page_dashboard()
