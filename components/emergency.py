575f46a1abd0a749851db0886ce695a3c52b8cbb
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-05-06 18:59:47 +0530

    Create emergency.py

---
+++ components/emergency.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from datetime import datetime
+
+
+def render_emergency(risk_level):
+
+    if risk_level == "Critical":
+
+        st.markdown("""
+        <div style='
+        background:#7f1d1d;
+        color:white;
+        padding:1rem;
+        border-radius:15px;
+        border:3px solid red;
+        '>
+        🚨 EMERGENCY ALERT ACTIVE 🚨
+        </div>
+        """, unsafe_allow_html=True)
+
+        st.error("📞 Emergency contact notification triggered")
+
+
+def render_timeline():
+
+    st.subheader("⏱ Emergency Response Timeline")
+
+    st.markdown(f"""
+    ✅ {datetime.now().strftime('%H:%M:%S')} - IoT vitals received
+
+    ☁ AWS synchronization completed
+
+    🧠 AI analysis generated
+
+    📨 Emergency monitoring active
+    """)
