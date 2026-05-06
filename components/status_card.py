2110a638573016e7732e355345122ad4abc70096
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-05-06 19:53:49 +0530

    Update status_card.py

--- components/status_card.py
+++ components/status_card.py
@@ -67,8 +67,20 @@ def render_status_card(risk_score, risk_level, risk_color, patient_status):
             }
         ))
 
-        score_fig.update_layout(height=320,paper_bgcolor="rgba(0,0,0,0)",
+        score_fig.update_layout(
+
+    height=320,
+
+    template="plotly_dark",
+
+    paper_bgcolor="rgba(0,0,0,0)",
+
     plot_bgcolor="rgba(0,0,0,0)",
-    font=dict(color="white"))
+
+    font=dict(
+        color="#0f172a",
+        size=22
+    )
+)
 
         st.plotly_chart(score_fig, use_container_width=True)
