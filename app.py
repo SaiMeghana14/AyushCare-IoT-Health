15b67a99a3cc9d0c8ed8d1bda5ecdfead7ba93ca
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-05-06 19:47:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from views.login import page_login
 from views.register import page_register
 from views.dashboard import page_dashboard
+from streamlit_autorefresh import st_autorefresh
 
 # --------------------------------------------------------------
 # PAGE CONFIG
@@ -12,6 +13,11 @@ st.set_page_config(
     page_icon="🩺",
     layout="wide",
     initial_sidebar_state="expanded"
+)
+
+st_autorefresh(
+    interval=5000,
+    key="live_refresh"
 )
 
 # --------------------------------------------------------------
