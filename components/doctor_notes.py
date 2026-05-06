41b492c3e63b5c635d5999716e731749fc6f7cb9
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-05-06 19:02:20 +0530

    Create doctor_notes.py

---
+++ components/doctor_notes.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def render_doctor_notes():
+
+    st.subheader("🩺 Doctor Notes")
+
+    st.info(
+        "Continue monitoring oxygen saturation and hydration levels."
+    )
