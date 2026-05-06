a8c4a5f7575a1ae926b9fa6497b0051253f73783
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-05-06 19:06:55 +0530

    Create login.py

---
+++ pages/login.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import time
+from services.auth_service import load_users
+
+
+def page_login():
+
+    st.markdown(
+        """
+        <h1 style='text-align:center;color:#0077b6;'>
+        🏥 AyushCare Login
+        </h1>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.markdown(
+        """
+        <p style='text-align:center;'>
+        AI-Powered Rural Healthcare Monitoring Platform
+        </p>
+        """,
+        unsafe_allow_html=True
+    )
+
+    username = st.text_input("👤 Username")
+    password = st.text_input("🔒 Password", type="password")
+
+    remember = st.checkbox("Remember Me")
+
+    users = load_users()
+
+    if st.button("🚀 Login"):
+
+        if username in users:
+
+            if password == users[username]["password"]:
+
+                st.success("✅ Login Successful")
+
+                st.session_state.current_user = users[username]["name"]
+
+                time.sleep(1)
+
+                st.session_state.page = "dashboard"
+
+                st.rerun()
+
+            else:
+                st.error("❌ Incorrect Password")
+
+        else:
+            st.error("❌ User Not Found")
+
+    st.write("---")
+
+    if st.button("🆕 Create New Account"):
+        st.session_state.page = "register"
+        st.rerun()
