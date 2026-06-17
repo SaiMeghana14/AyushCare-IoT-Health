4306f6eb18837a0e181a4f8db5762ceef9a98c9f
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-06-17 13:02:14 +0530

    Update data_loader.py

--- services/data_loader.py
+++ services/data_loader.py
@@ -16,7 +16,40 @@ def upload_csv():
     )
 
     if uploaded:
+
         df = pd.read_csv(uploaded)
-        st.subheader("Uploaded CSV Data")
-        st.dataframe(df)
-        st.stop()
+
+        df["timestamp"] = pd.to_datetime(
+            df["timestamp"]
+        )
+
+        patients = df["patient_id"].unique().tolist()
+
+        selected = st.sidebar.selectbox(
+            "👤 Select Patient",
+            patients
+        )
+
+        patient_df = (
+            df[df["patient_id"] == selected]
+            .sort_values("timestamp")
+        )
+
+        latest = patient_df.iloc[-1]
+
+        vitals = {
+            "heart_rate": latest["heart_rate"],
+            "spo2": latest["spo2"],
+            "temperature": latest["temperature"],
+            "bp": latest["bp"],
+            "respiratory_rate": latest["respiratory_rate"]
+        }
+
+        with st.expander(
+            "📄 View Uploaded CSV Data"
+        ):
+            st.dataframe(df)
+
+        return vitals, selected, df
+
+    return None, None, None
